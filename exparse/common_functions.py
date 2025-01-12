e5ae9bb5d3c237bac05fb45f0d0bc5013c460f0f
Niall O'Callaghan <nocalla@users.noreply.github.com>
2025-01-12 00:33:45 +0000

    Add file_to_dataframe to common_functions

--- exparse/common_functions.py
+++ exparse/common_functions.py
@@ -1,6 +1,29 @@
 import re
 from pathlib import Path
 
+def file_to_dataframe(
+    file: Path, headings: list[str], id:str, replace: list[tuple[str, str]]
+):
+    # Regex patterns to match headers and blank lines
+    COMMON_CLEANUP_REGEX = [
+        (r"^\s*\n", ""),
+        (r"\*LIVE\* .*\n.*\n.*", ""),
+        (r"\*LSTD\* .*\n.*\n.*", ""),
+        (r"\*TEST\* .*\n.*\n.*", ""),
+        (r"\*TSTD\* .*\n.*\n.*", ""),
+    ]
+    # Read the text file
+    with open(file, "r") as f:
+        data = f.read()
+
+    # cleanup file
+    patterns = COMMON_CLEANUP_REGEX + replace
+    data = regex_substitution(data, patterns)
+    debug_test_current_data(data)
+    # convert to dataframe
+    df = text_data_to_dataframe(text=data, id=id, headings=headings)
+    return df
+
 
 def regex_substitution(text: str, substitutions: list[tuple]) -> str:
     # Apply all match subtitutions to the text
